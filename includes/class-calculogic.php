76796553d8e394cd12ee92a2a623743fb04ca46a
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-02-18 19:03:36 +0000

    Refactored Form Builder and Added Core Builder Tabs

--- includes/class-calculogic.php
+++ includes/class-calculogic.php
@@ -31,43 +31,10 @@ namespace Calculogic\Includes;
  */
 class Calculogic {
 
-    /**
-     * The loader that's responsible for maintaining and registering all hooks that power
-     * the plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      Calculogic_Loader    $loader    Maintains and registers all hooks for the plugin.
-     */
     protected $loader;
-
-    /**
-     * The unique identifier of this plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-     */
     protected $plugin_name;
-
-    /**
-     * The current version of the plugin.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      string    $version    The current version of the plugin.
-     */
     protected $version;
 
-    /**
-     * Define the core functionality of the plugin.
-     *
-     * Set the plugin name and the plugin version that can be used throughout the plugin.
-     * Load the dependencies, define the locale, and set the hooks for the admin area and
-     * the public-facing side of the site.
-     *
-     * @since    1.0.0
-     */
     public function __construct() {
         if ( defined( 'CALCULOGIC_VERSION' ) ) {
             $this->version = CALCULOGIC_VERSION;
@@ -80,25 +47,8 @@ class Calculogic {
         $this->set_locale();
         $this->define_admin_hooks();
         $this->define_public_hooks();
-
     }
 
-    /**
-     * Load the required dependencies for this plugin.
-     *
-     * Include the following files that make up the plugin:
-     *
-     * - Calculogic_Loader. Orchestrates the hooks of the plugin.
-     * - Calculogic_i18n. Defines internationalization functionality.
-     * - Calculogic_Admin. Defines all hooks for the admin area.
-     * - Calculogic_Public. Defines all hooks for the public side of the site.
-     *
-     * Create an instance of the loader which will be used to register the hooks
-     * with WordPress.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
     private function load_dependencies() {
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-calculogic-loader.php';
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-calculogic-i18n.php';
@@ -114,30 +64,6 @@ class Calculogic {
         $this->loader = new \Calculogic\Includes\Calculogic_Loader();
     }
 
-    /**
-     * Define the locale for this plugin for internationalization.
-     *
-     * Uses the Calculogic_i18n class in order to set the domain and to register the hook
-     * with WordPress.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
-    private function set_locale() {
-
-        $plugin_i18n = new \Calculogic\Includes\Calculogic_i18n();
-
-        $this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-
-    }
-
-    /**
-     * Register all of the hooks related to the admin area functionality
-     * of the plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
     private function define_admin_hooks() {
         $plugin_admin = new \Calculogic\Admin\Calculogic_Admin( $this->get_plugin_name(), $this->get_version() );
         $plugin_builder = new \Calculogic\Includes\Calculogic_Builder();
@@ -153,13 +79,6 @@ class Calculogic {
         $this->loader->add_action( 'save_post', '\Calculogic\Includes\Calculogic_CPT', 'save_meta_boxes' );
     }
 
-    /**
-     * Register all of the hooks related to the public-facing functionality
-     * of the plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     */
     private function define_public_hooks() {
         $plugin_public = new \Calculogic\Public\Calculogic_Public( $this->get_plugin_name(), $this->get_version() );
 
@@ -173,44 +92,19 @@ class Calculogic {
         $this->loader->add_action( 'init', '\Calculogic\Includes\Calculogic_Form_Handler', 'handle_form_submission' );
     }
 
-    /**
-     * Run the loader to execute all of the hooks with WordPress.
-     *
-     * @since    1.0.0
-     */
     public function run() {
         $this->loader->run();
     }
 
-    /**
-     * The name of the plugin used to uniquely identify it within the context of
-     * WordPress and to define internationalization functionality.
-     *
-     * @since     1.0.0
-     * @return    string    The name of the plugin.
-     */
     public function get_plugin_name() {
         return $this->plugin_name;
     }
 
-    /**
-     * The reference to the class that orchestrates the hooks with the plugin.
-     *
-     * @since     1.0.0
-     * @return    Calculogic_Loader    Orchestrates the hooks of the plugin.
-     */
     public function get_loader() {
         return $this->loader;
     }
 
-    /**
-     * Retrieve the version number of the plugin.
-     *
-     * @since     1.0.0
-     * @return    string    The version number of the plugin.
-     */
     public function get_version() {
         return $this->version;
     }
-
 }
