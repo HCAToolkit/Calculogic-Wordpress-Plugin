beb776149a5d0bfaab994f92a7c23af6569fd065
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-02-26 15:10:06 +0000

    reverted

--- calculogic.php
+++ calculogic.php
@@ -81,8 +81,3 @@ function run_calculogic() {
     $plugin->run();
 }
 run_calculogic();
-
-add_action('admin_menu', function() {
-    $builder = new \Calculogic\Includes\Calculogic_Builder();
-    $builder->add_builder_menu();
-});
