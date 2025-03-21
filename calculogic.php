5a81e3d920d9d7029c395a987fe8b79570ca5843
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-03-21 22:43:55 +0000

    activator try 2

--- calculogic.php
+++ calculogic.php
@@ -39,14 +39,11 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/class-calculogic-deactivato
 
 /**
  * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
 define( 'CALCULOGIC_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-calculogic-activator.php
  */
 function activate_calculogic() {
     Calculogic_Activator::activate();
@@ -54,7 +51,6 @@ function activate_calculogic() {
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-calculogic-deactivator.php
  */
 function deactivate_calculogic() {
     Calculogic_Deactivator::deactivate();
@@ -80,12 +76,6 @@ add_action('widgets_init', 'register_calculogic_form_widget');
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
  */
 function run_calculogic() {
     $plugin = new \Calculogic\Includes\Calculogic();
--- includes/class-calculogic-activator.php
+++ includes/class-calculogic-activator.php
@@ -1,16 +1,6 @@
 <?php
 
 /**
- * Fired during plugin activation
- *
- * @link       https://www.hcatoolkit.com
- * @since      1.0.0
- *
- * @package    Calculogic
- * @subpackage Calculogic/includes
- */
-
-/**
  * Fired during plugin activation.
  *
  * This class defines all code necessary to run during the plugin's activation.
@@ -18,11 +8,7 @@
  * @since      1.0.0
  * @package    Calculogic
  * @subpackage Calculogic/includes
- * @author     H-CAT <yuri.bara.karmas.sin@gmail.com>
  */
-
- namespace Calculogic\Includes;
-
 class Calculogic_Activator {
 
     /**
@@ -33,5 +19,4 @@ class Calculogic_Activator {
     public static function activate() {
         // Add activation logic here, such as creating database tables or setting default options.
     }
-
 }
